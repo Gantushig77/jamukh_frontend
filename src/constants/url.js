d0a743477e803be30ded7693e1db3fcfe92d8339
temuujin100 <temuujin961009@gmail.com>
2022-01-28 08:00:35 +0800

    20220128

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,8 +1,8 @@
 const DEV = process.env.NODE_ENV !== 'production';
 const backend_port = '4000';
-const base_url = `http://192.168.1.26:${backend_port}`;
-const img_url = `http://192.168.1.26:${backend_port}/image/`;
-const membership_img_url = `http://192.168.1.26:${backend_port}/membership/`;
+const base_url = DEV === true ? `http://192.168.1.26:${backend_port}`:`http://43.231.114.19:${backend_port}`;
+const img_url = DEV === true ?`http://192.168.1.26:${backend_port}/image/`:`http://43.231.114.19:${backend_port}/image/`;
+const membership_img_url = DEV === true ?`http://192.168.1.26:${backend_port}/membership/`:`http://43.231.114.19:${backend_port}/membership/`;
 
 const url = {
   general: ['/antiquest', '/estate', '/cars', '/property','/painting'],
